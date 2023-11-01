45119d1c37757df9335d1907c461023c77c860a7
Jigyashu <jigyashusingh01@gmail.com>
2023-11-01 20:30:33 +0530

    ImageVideo Access in OpenCV

---
+++ MembersCode/ReadingImagesVideos/ImageAccess.py
@@ -0,0 +1,27 @@
+#Importing teh library
+import sys
+import cv2
+
+
+#getting teh image from system
+# The image has to be stored at same location as file
+#By default it reads image in BGR format
+image = cv2.imread(r"nature.jpg")
+
+#To read image in grayscale format 
+image_gray = cv2.imread(r"nature.jpg",cv2.IMREAD_GRAYSCALE)
+
+#To read images as it is (With alha mask or something else)
+image_as = cv2.imread(r"nature.jpg",cv2.IMREAD_UNCHANGED)
+
+
+#To display teh image
+cv2.imshow('window name', image)
+cv2.imshow('Grayscale image', image_gray)
+cv2.imshow('Unchanged iamge', image_as)
+
+#Now we have to hold above created windows other wise program will end
+#The parameter passed is Time in milliseconds
+cv2.waitKey(5000)
+
+cv2.destroyAllWindows()
